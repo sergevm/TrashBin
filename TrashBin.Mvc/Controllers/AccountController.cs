75c80b30f443b5f9954e020d76962e7989d5caa8
sergevm <serge.vanmeerbeeck@gmail.com>
2013-08-06 22:39:17 +0200

    Added Twitter bootstrap (very basic integration)

--- TrashBin.Mvc/Controllers/AccountController.cs
+++ TrashBin.Mvc/Controllers/AccountController.cs
@@ -54,7 +54,7 @@ namespace TrashBin.Mvc.Controllers
         {
             WebSecurity.Logout();
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Company");
         }
 
         //
@@ -81,7 +81,7 @@ namespace TrashBin.Mvc.Controllers
                 {
                     WebSecurity.CreateUserAndAccount(model.UserName, model.Password);
                     WebSecurity.Login(model.UserName, model.Password);
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Company");
                 }
                 catch (MembershipCreateUserException e)
                 {
@@ -337,7 +337,7 @@ namespace TrashBin.Mvc.Controllers
             }
             else
             {
-                return RedirectToAction("Index", "Home");
+                return RedirectToAction("Index", "Company");
             }
         }
 
---
+++ TrashBin.Mvc/Controllers/CompanyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using NServiceBus;
+using TrashBin.Messages;
+
+namespace TrashBin.Mvc.Controllers
+{
+    public class CompanyController : Controller
+    {
+        private readonly IBus _bus;
+
+        public CompanyController(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Companies";
+            return View();
+        }
+    }
+}
